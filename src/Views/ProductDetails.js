cafbd661a419cb7a62e0bf8391a99543c89a0c8b
Leon-A1 <leonaviev@gmail.com>
2021-02-09 22:23:38 +0200

    design and server updates

--- src/Views/ProductDetails.js
+++ src/Views/ProductDetails.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+// import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import "../Assets/css/views/product-details.css";
 import NavBar from "../Components/NavBar/NavBar";
 
 export default function ProductDetails() {
   let { productid } = useParams();
 
-  const [productName, setProductName] = useState("");
-  const [productDescription, setProductDescription] = useState("");
-  const [productImage, setProductImage] = useState("");
+  // const [productName, setProductName] = useState("");
+  // const [productDescription, setProductDescription] = useState("");
+  // const [productImage, setProductImage] = useState("");
   console.log("BEFORE USE EFFECT:", productid);
 
-  const fetchItems = async () => {
-    const result = await axios(
-      // `https://www.breakingbadapi.com/api/characters?name=${query}`
-      `http://localhost:5000/api/get_product${productid}`
-    );
-    console.log(result.data);
-  };
+  // const fetchItems = async () => {
+  //   const result = await axios(
+  //     // `https://www.breakingbadapi.com/api/characters?name=${query}`
+  //     `http://localhost:5000/api/get_product${productid}`
+  //   );
+  //   console.log(result.data);
+  // };
 
   // useEffect(() => {
   //   var product_data = axios(
@@ -41,9 +42,9 @@ export default function ProductDetails() {
         <h1>product details</h1>
 
         <h3>Product ID: {productid}</h3>
-        <h3>Product Name: {productName}</h3>
-        <h3>Product Description: {productDescription}</h3>
-        <img src={productImage} alt=""></img>
+        {/* <h3>Product Name: {productName}</h3> */}
+        {/* <h3>Product Description: {productDescription}</h3> */}
+        {/* <img src={productImage} alt=""></img> */}
         {/* {productImage} */}
       </div>
     </div>
